84edf43d190bb3adf5c9b82446b6028808ea494b
j.s <jsaylor@brandeis.edu>
2019-04-05 16:05:39 -0400

    initial attempt
    logging middleware
    needs export statement

---
+++ controllers/Logger.js
@@ -0,0 +1,46 @@
+'use strict';
+fs = require('fs');
+
+function generateLogPathFile() {
+    var dt = new Date().toISOString();
+
+    var year = dt.getFullYear().toString();
+    var rawMonth = dt.getMonth() + 1;
+    var month = rawMonth.toString().padStart(2, '0');
+    var day = dt.getDate().toString().padStart(2, '0');
+    var fileNameParts = [
+        'filescan',
+        year,
+        month,
+        day
+    ];
+    var fileName = fileNameParts.join('-');
+    // FIXME: hardcoding base like this is sub-optimal
+    var logPathParts = [
+        '/var/data/filescan/archive',
+        year,
+        month,
+        filename
+    ];
+    var logPath = logPathParts.join('/');
+    return {path: logPath, file: fileName};
+}
+
+function logger(req, res, next) {
+    var components = [
+        d,
+        res.filename,
+        res.numPages,
+        res.numPagesChecked
+    ];
+    var logLine = components.join(' ');
+    var log = generateLogPathFile();
+    var pathFile = log[path] + '/' + log[file];
+    fs.mkdirSync(log[path], { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+    fs.appendFile(pathFile, logLine, (err) => {
+        if(err) throw err;
+    });
+    next();
+}
