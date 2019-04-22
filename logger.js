81a320b5f750801afa5e2fcf1d67e09194ba86d6
j.s <jsaylor@brandeis.edu>
2019-04-22 15:09:35 -0400

    logging working now
    logging library added file path creating function
    also requested URL
    index.js now calls logging lib appropriately
    swagger.yaml indentation fixed

--- logger.js
+++ logger.js
@@ -6,16 +6,19 @@ exports.logActivity = function (req, res, next) {
     var timestamp = dt.toISOString();
     var components = [
         timestamp,
+        req.url,
         res.filename,
         res.numPages,
         res.numPagesChecked
     ];
     var logLine = components.join(' ');
+    logLine += '\n';
     var log = generateLogPathFile(dt);
-    var pathFile = log[path] + '/' + log[file];
-    fs.mkdirSync(log[path], {recursive: true}, (err) => {
-        if (err) throw err;
-    });
+    var pathFile = log['path'] + '/' + log['file'];
+    validatePath(log['path']);
+    // fs.mkdirSync(log['path'], { recursive: true }, (err) => {
+    //    if (err) throw err;
+    //});
     fs.appendFile(pathFile, logLine, (err) => {
         if(err) throw err;
     });
@@ -25,8 +28,14 @@ exports.logActivity = function (req, res, next) {
 function generateLogPathFile(dt) {
     var year = dt.getFullYear().toString();
     var rawMonth = dt.getMonth() + 1;
-    var month = rawMonth.toString().padStart(2, '0');
-    var day = dt.getDate().toString().padStart(2, '0');
+    var month = rawMonth.toString();
+    if(month.length < 2) {
+        month = '0' + month;
+    } 
+    var day = dt.getDate().toString();
+    if(day.length < 2) {
+        day = '0' + day;
+    } 
     var fileNameParts = [
         'filescan',
         year,
@@ -38,10 +47,24 @@ function generateLogPathFile(dt) {
     var logPathParts = [
         '/var/data/filescan/archive',
         year,
-        month,
-        filename
+        month
     ];
     var logPath = logPathParts.join('/');
     return {path: logPath, file: fileName};
+}
+
+function validatePath(p) {
+    var pathParts = p.split('/');
+    var currentPath = '/';
+    for(var i = 0; i < pathParts.length; i++) {
+        if(pathParts[i].length < 1) {
+            continue;
+        }
+        currentPath += pathParts[i] + '/';
+        if(! fs.existsSync(currentPath)) {
+            fs.mkdirSync(currentPath);
+        }
+    }
+    return true;
 }
 
